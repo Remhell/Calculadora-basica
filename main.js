3a02f53b675ddc0d3f0bb15616bb862dc917122c
Alejandro Muñoz <161650990+Remhell@users.noreply.github.com>
2025-02-20 15:15:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ buttons.forEach((btn) => {
   btn.addEventListener("click", () => {
     if (btn.id === "=") {
       display.value = eval(display.value);
-      console.log(eval(display.value))
+      console.log(eval(display.value));
     } else if (btn.id === "clear") {
       display.value = "";
     } else if (btn.id === "delete") {
